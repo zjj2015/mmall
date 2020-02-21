1bda5a3b4644fd8a3354a79debb97f6a6a0d4a03
zhangjj <1632091787@qq.com>
2020-02-21 14:57:43 +0800

    支付宝集成

---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,13 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
+public interface IOrderService {
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    ServerResponse aliCallbak(Map<String,String> params);
+
+    ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
+}
